1f2d417581c9c9ef34624f7223d22d85c814276e
Kerem Yemenici <nxmles@gmail.com>
2022-12-03 22:09:27 +0300

    The project has been started

---
+++ kayra.py
@@ -0,0 +1,51 @@
+# This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+# To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/
+
+# -*- coding:UTF-8 -*-
+
+import os
+import sys
+import time
+import json
+import sqlite3
+import argparse
+import subprocess
+from gtts import gTTS
+import speech_recognition as sr
+
+class Kayra():
+    def __init__(self):
+        pass
+
+    def listen(self):
+        pass
+
+    def speak(self):
+        pass
+
+    def compile(self):
+        pass
+
+    def execute(self):
+        pass
+
+    def connectDB(self):
+        pass
+
+    def connnectCursor(self):
+        pass
+
+    def getData(self):
+        pass
+
+    def closeDB(self):
+        pass
+
+    def openGUI(self):
+        pass
+
+    def closeGUI(self):
+        pass
+
+    def quit(self):
+        pass
